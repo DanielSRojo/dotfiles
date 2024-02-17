5cc3f045bdae1ada42aee91374dbfce1002d9cd3
Daniel S. Rojo <danielsrojo@protonmail.com>
2024-02-17 18:48:21 +0100

    Shift to abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Enable starship prompt
-starship init fish | source
-
-# Enable transient promt
-enable_transience
+# Enable vi style motion
+fish_vi_key_bindings
 
 # Add custom paths to fish_user_paths
 set -U fish_user_paths /usr/local/go/bin $fish_user_paths
@@ -14,12 +11,17 @@ set -U fish_user_paths $HOME/go/bin $fish_user_paths
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
+# Import custom aliases
+source ~/.config/fish/alias.sh
+source ~/.config/fish/abbr.sh
+source ~/.config/fish/corporate.sh
 
 # Set up zoxide
 zoxide init fish | source
 
-# Enable vi style motion
-fish_vi_key_bindings
+# Enable starship prompt
+starship init fish | source
+enable_transience
 
-# Import custom aliases
-source ~/.config/fish/alias.sh
+# Kubectl completion
+kubectl completion fish | source
