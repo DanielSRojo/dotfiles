cd79ab37b55c4255194a07ed44c550d30529dea7
Daniel S. Rojo <danielsrojo@protonmail.com>
2024-02-17 18:05:21 +0100

    Add go path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,8 @@ enable_transience
 
 # Add custom paths to fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+set -U fish_user_paths $HOME/go/bin $fish_user_paths
+set -U fish_user_paths /usr/local/go/bin $fish_user_paths
 
 # Set up zoxide
 zoxide init fish | source
