56bed7b70e2f1435febc1750683175771ed55b5c
Daniel S. Rojo <danielsrojo@protonmail.com>
2024-02-17 18:00:02 +0100

    Add nvim config files

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,200 @@
+local plugins = {
+
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "golines",
+        "goimports",
+        "gomodifytags",
+        "rust-analyzer",
+      },
+    },
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings("dap")
+    end
+  },
+
+  {
+    "dreamsofcode-io/nvim-dap-go",
+    ft = "go",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      local dap = require('dap')
+      dap.configurations.go = {
+       {
+         name = "Launch cmd/main.go",
+         type = "go",
+         request = "launch",
+         mode = "debug",
+         program = "${workspaceFolder}/cmd/main.go",
+      },
+    }
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function (_, opts)
+      require('rust-tools').setup(opts)
+    end,
+  },
+
+  {
+    'saecki/crates.nvim',
+    ft = {"rust", "toml"},
+    config = function (_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function ()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    -- ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      "BufReadPre " .. vim.fn.expand "~" .. "/.obsidian/**.md",
+      "BufNewFile " .. vim.fn.expand "~" .. "/.obsidian/**.md",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/.obsidian/",
+        },
+      },
+    },
+  },
+
+  -- {
+  --   "nvim-neorg/neorg",
+  --   build = ":Neorg sync-parsers",
+  --   lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
+  --   -- tag = "*",
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   config = function()
+  --     require("neorg").setup {
+  --       load = {
+  --         ["core.defaults"] = {}, -- Loads default behaviour
+  --         ["core.concealer"] = {}, -- Adds pretty icons to your documents
+  --         ["core.dirman"] = { -- Manages Neorg workspaces
+  --           config = {
+  --             workspaces = {
+  --               notes = "~/notes",
+  --             },
+  --           },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
+
+  -- {
+  -- "ray-x/go.nvim",
+  -- dependencies = {  -- optional packages
+  --   "ray-x/guihua.lua",
+  --   "neovim/nvim-lspconfig",
+  --   "nvim-treesitter/nvim-treesitter",
+  -- },
+  -- config = function()
+  --   require("go").setup()
+  -- end,
+  -- event = {"CmdlineEnter"},
+  -- ft = {"go", 'gomod'},
+  -- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  -- },
+}
+return plugins
