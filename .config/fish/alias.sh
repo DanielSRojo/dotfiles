8803bc36c0052e776589b2ea104d8d67dc70a2fb
Daniel S. Rojo <danielsrojo@protonmail.com>
2024-02-19 13:51:31 +0100

    Add alias file

---
+++ .config/fish/alias.sh
@@ -0,0 +1,11 @@
+## System
+alias grn='grep -rni'
+alias code='codium'
+alias curlhc='curl -s -o /dev/null -I -w "%{http_code}"'
+alias clip='wl-copy'
+alias ls='eza'
+alias cat='bat -p'
+alias cy='bat --language=yaml'
+alias fvim='fzf --print0 | xargs -0 -o vim'
+alias vim=nvim
+
