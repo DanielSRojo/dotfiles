302fd3307449e9b9e3eab341fc1bc6f121b840df
Daniel S. Rojo <danielsrojo@protonmail.com>
2024-03-04 10:12:10 +0100

    Set fish configuration into interactive module

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,47 +1,50 @@
+# Commands to run in interactive sessions can go here
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+
+  # Remove fish greeting
+  set fish_greeting
+
+  # Set nvim as editor
+  set -x EDITOR nvim
+
+  # Enable vi style motion
+  fish_vi_key_bindings
+  
+  # Add custom paths to fish_user_paths
+  set -U fish_user_paths /usr/local/go/bin $fish_user_paths
+  set -U fish_user_paths $HOME/go/bin $fish_user_paths
+  set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+  set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+  set -U fish_user_paths $HOME/.krew/bin $fish_user_paths
+  
+  # Add GOPATH
+  set -x GOPATH ~/go
+  
+  # Import custom aliases
+  source ~/.config/fish/alias.sh
+  source ~/.config/fish/abbr.sh
+  source ~/.config/fish/corporate.sh
+  
+  # Import custom functions
+  source ~/.config/fish/functions/practice.fish
+  
+  # Set up zoxide
+  zoxide init fish | source
+  
+  # Enable starship prompt
+  starship init fish | source
+  enable_transience
+  
+  # Kubectl completion
+  kubectl completion fish | source
+  
+  # Helm completion
+  helm completion fish | source
+  
+  # Openstack completion
+  openstack complete --shell=fish | source
+  
+  # Direnv hook
+  direnv hook fish | source
+
 end
-
-# Remove fish greeting
-set fish_greeting
-
-# Set nvim as editor
-set -x EDITOR nvim
-
-# Enable vi style motion
-fish_vi_key_bindings
-
-# Add custom paths to fish_user_paths
-set -U fish_user_paths /usr/local/go/bin $fish_user_paths
-set -U fish_user_paths $HOME/go/bin $fish_user_paths
-set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
-set -U fish_user_paths $HOME/.krew/bin $fish_user_paths
-
-# Add GOPATH
-set -x GOPATH ~/go
-
-# Import custom aliases
-source ~/.config/fish/alias.sh
-source ~/.config/fish/abbr.sh
-source ~/.config/fish/corporate.sh
-
-# Set up zoxide
-zoxide init fish | source
-
-# Enable starship prompt
-starship init fish | source
-enable_transience
-
-# Kubectl completion
-kubectl completion fish | source
-
-# Helm completion
-helm completion fish | source
-
-# Openstack completion
-openstack complete --shell=fish | source
-
-# Direnv hook
-direnv hook fish | source
-
