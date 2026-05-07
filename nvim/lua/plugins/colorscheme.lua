e61079816bd3e7aae9a634a2c45642bd9ebfba8f
Daniel S. Rojo <danielsrojo@protonmail.com>
2026-05-07 18:48:29 +0200

    chore: switch to Flexoki Light theme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  -- add flexoki
+  {
+    "kepano/flexoki-neovim",
+    name = "flexoki",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = false,
+    },
+    config = function(_, opts)
+      require("flexoki").setup(opts)
+    end,
+  },
+
+  -- Configure LazyVim to load flexoki
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "flexoki-dark",
+      colorscheme = "flexoki-light",
+    },
+  },
+}
